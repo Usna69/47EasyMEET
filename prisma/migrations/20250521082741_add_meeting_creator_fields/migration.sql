8edf046d129c355b210b54adafeb3527a88c52af
Usna69 <brian.kiprono.n@gmail.com>
2025-05-23 23:34:29 +0300

    ,,

--- prisma/migrations/20250521082741_add_meeting_creator_fields/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Meeting" ADD COLUMN "creatorEmail" TEXT;
-ALTER TABLE "Meeting" ADD COLUMN "creatorType" TEXT;
-ALTER TABLE "Meeting" ADD COLUMN "meetingId" TEXT;
-ALTER TABLE "Meeting" ADD COLUMN "sector" TEXT;
--- prisma/migrations/20250521153925_add_organization_and_signature/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "Attendee" ADD COLUMN "organization" TEXT;
-ALTER TABLE "Attendee" ADD COLUMN "phoneNumber" TEXT;
-ALTER TABLE "Attendee" ADD COLUMN "signatureData" TEXT;
